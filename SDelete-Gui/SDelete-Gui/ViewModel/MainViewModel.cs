0e136623694bdcfaae0ed016a1150905653acfa9
Ricardo Polo <ricardo.polo@tulpep.com>
2014-08-04 11:56:49 -0500

    Method to add context menu to all files

--- SDelete-Gui/SDelete-Gui/ViewModel/MainViewModel.cs
+++ SDelete-Gui/SDelete-Gui/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
 using System;
+using Microsoft.Win32;
 
 namespace SDelete_Gui.ViewModel
 {
@@ -26,7 +27,38 @@ namespace SDelete_Gui.ViewModel
 
         private void ExecuteInstall()
         {
-            throw new NotImplementedException();
+            AddContextMenuToFiles("prueba", "notepad.exe %1");
+        }
+
+
+        private bool AddContextMenuToFiles(string name, string command)
+        {
+            RegistryKey regmenu = null;
+            RegistryKey regcmd = null;
+            bool result;
+            try
+            {
+                const string menuName = "*\\shell\\";
+                string keyName = menuName + name;
+                regmenu = Registry.ClassesRoot.CreateSubKey(keyName);
+                regcmd = Registry.ClassesRoot.CreateSubKey(keyName + "\\command");
+                regcmd.SetValue("", command);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (regmenu != null)
+                    regmenu.Dispose();
+                if (regcmd != null)
+                    regcmd.Dispose();
+            }
+
+            return result;
+
         }
     }
 }
