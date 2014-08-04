1a50580a961a8b50cc920e756416b04c89b89583
Ricardo Polo <ricardo.polo@tulpep.com>
2014-08-04 10:56:21 -0500

    Mahappa metro window

--- SDelete-Gui/SDelete-Gui/MainWindow.xaml.cs
+++ SDelete-Gui/SDelete-Gui/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace SDelete_Gui
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
